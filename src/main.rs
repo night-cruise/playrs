5c7410dcff04b8a669f11ae011670528f108c57a
Night Cruising <2586447362@qq.com>
2022-01-29 22:31:24 +0800

    Use eprintln to print error

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use structopt::StructOpt;
 fn main() {
     let args = playrs::cli::Opt::from_args();
     if let Err(err) = playrs::run(args) {
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
         std::process::exit(1);
     }
 }
